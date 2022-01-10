7b3c768d4ab45771f65f99d0d9434f22476dfb9f
kimjinsub <hikm9985@naver.com>
2022-01-10 19:59:05 +0900

    UI 구성

---
+++ app/src/main/java/com/example/toyproject006_pomodorotimer/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.toyproject006_pomodorotimer
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private val remainMinutesTextView: TextView by lazy {
+        findViewById(R.id.remainMinuteTextView)
+    }
+
+    private val seekbar: SeekBar by lazy {
+        findViewById(R.id.seekBar)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bindViews()
+    }
+
+    private fun bindViews() {
+        seekbar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                @SuppressLint("SetTextI18n")
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    remainMinutesTextView.text = "%02d".format(progress)
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+                }
+
+
+            }
+        )
+    }
+}
