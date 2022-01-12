deb433b0748517da14c941473fe83556e9695aa1
kimjinsub <hikm9985@naver.com>
2022-01-12 20:18:19 +0900

    soundPool로 효과음 넣기

--- app/src/main/java/com/example/toyproject006_pomodorotimer/MainActivity.kt
+++ app/src/main/java/com/example/toyproject006_pomodorotimer/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.toyproject006_pomodorotimer
 
 import android.annotation.SuppressLint
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -21,13 +22,36 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.seekBar)
     }
 
+    private val soundPool = SoundPool.Builder().build()
+
     private var currentCountDownTimer: CountDownTimer? = null
+
+    private var tickingSoundId: Int? = null
+    private var bellSoundId: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bindViews()
+        initSounds()
+    }
+
+    //    다시 App으로 돌아올 때 다시 재생한다.
+    override fun onResume() {
+        super.onResume()
+        soundPool.autoResume()
+    }
+
+    //    App이 화면에서 안보일 경우 sound를 일시정지 시킨다.
+    override fun onPause() {
+        super.onPause()
+        soundPool.autoPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        soundPool.release()
     }
 
     private fun bindViews() {
@@ -38,7 +62,7 @@ class MainActivity : AppCompatActivity() {
                     progress: Int,
                     fromUser: Boolean
                 ) {
-                    if(fromUser) {
+                    if (fromUser) {
                         updateRemainTime(progress * 60 * 1000L)
                     }
                 }
@@ -50,16 +74,20 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
                     seekBar ?: return
-                    currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
-                    currentCountDownTimer?.start()
+                    startCountDown()
                 }
-
-
             }
         )
     }
 
-//  카운트다운 메서드 : 몇 millisecond 뒤에 받을건지를 인자로 받게 됨
+    //  사운드 메서드
+    private fun initSounds() {
+        tickingSoundId = soundPool.load(this, R.raw.timer_ticking, 1)
+        bellSoundId = soundPool.load(this, R.raw.timer_bell, 1)
+    }
+
+
+    //  카운트다운 메서드 : 몇 millisecond 뒤에 받을건지를 인자로 받게 됨
     private fun createCountDownTimer(initialMillis: Long) =
         object : CountDownTimer(initialMillis, 1000L) {
             override fun onTick(millisUntilFinished: Long) {
@@ -69,12 +97,36 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                updateRemainTime(0)
-                updateSeekBar(0)
+                completeCountDown()
             }
         }
 
-//  매 1초 마다 UI를 갱신해야 하기 때문에 분 과 초를 갱신하는 메서드
+    private fun startCountDown() {
+
+
+        currentCountDownTimer = createCountDownTimer(seekbar.progress * 60 * 1000L)
+        currentCountDownTimer?.start()
+
+/*                인자로 전달해야할 프로퍼티가 Nullable 할 경우에 Null이 아닌 경우 let으로 주면서
+                  soundId로 해당 값을 전달하고 그 값을 바로 인자로 전달하는 방식으로 해서 Null이
+                  아닌 경우에만 이 메서드를 호출하는 코드 */
+        tickingSoundId?.let { soundId ->
+            soundPool.play(soundId, 1F, 1F, 0, -1, 1F)
+        }
+
+    }
+
+    private fun completeCountDown() {
+        updateRemainTime(0)
+        updateSeekBar(0)
+
+        soundPool.autoPause()
+        bellSoundId?.let { soundId ->
+            soundPool.play(soundId, 1F, 1F, 0, 0, 1F)
+        }
+    }
+
+    //  매 1초 마다 UI를 갱신해야 하기 때문에 분 과 초를 갱신하는 메서드
     @SuppressLint("SetTextI18n")
     private fun updateRemainTime(remainMillis: Long) {
         val remainSeconds = remainMillis / 1000
@@ -82,8 +134,8 @@ class MainActivity : AppCompatActivity() {
         remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
     }
 
-//  SeekBar 1분이 지날때 마다 1칸씩 줄어드는 메서드
-   private fun updateSeekBar(remainMillis: Long) {
+    //  SeekBar 1분이 지날때 마다 1칸씩 줄어드는 메서드
+    private fun updateSeekBar(remainMillis: Long) {
         seekbar.progress = (remainMillis / 1000 / 60).toInt()
     }
 }
